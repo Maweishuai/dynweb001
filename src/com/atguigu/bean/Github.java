bdadb0a1bf594f116299dae8b58ab8d7dfc92acf
Maweishuai <1377650453@qq.com>
2020-02-28 00:09:46 +0800

    Initial commit

---
+++ src/com/atguigu/bean/Github.java
@@ -0,0 +1,11 @@
+package com.atguigu.bean;
+
+/**
+ * @author Administrator
+ * @create 2020-02-28 0:05
+ */
+public class Github {
+    public static void main(String[] args) {
+        System.out.println("11111");
+    }
+}
